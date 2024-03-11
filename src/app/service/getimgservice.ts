5a7e83cb4722c2c9d90474356238714c8657d2dc
Avatarui <162191182+Avatarui@users.noreply.github.com>
2024-03-11 23:34:56 +0700

    getimgservice.ts

--- src/app/service/getimgservice.ts
+++ src/app/service/getimgservice.ts
@@ -4,45 +4,52 @@ import { lastValueFrom } from 'rxjs';
 import { GetImg } from '../model/img';
 import { Constants } from '../config/constans';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Getimgservice {
+  constructor(private constants: Constants, private http: HttpClient) {}
+  public async Getimg() {
+    const url = this.constants.API_ENDPOINT + '/picture';
+    const response = await lastValueFrom(this.http.get(url));
+    return response as GetImg[];
+  }
 
-
-      constructor(private constants : Constants , private http : HttpClient){}
-      public async Getimg() {
-        const url = this.constants.API_ENDPOINT + "/picture";
-        const response = await lastValueFrom(this.http.get(url));
-        return response as GetImg[];
-      }
-
-      public async InsertVote(uid: number, picture_id: number, point: number, isWinner: boolean ) {
-        const url = this.constants.API_ENDPOINT + '/vote/insertimg';
-        const body = {
-          userid: uid,
-          imgid: picture_id,
-          score: point,
-          isWinner: isWinner,
-        };
-        const headers = new HttpHeaders().set('Content-Type', 'application/json');
-        try {
-          const response = await this.http.post(url, body, { headers: headers }).toPromise();
-          return true;
-        } catch (error) {
-          throw error;
-        }
-      }
-      public async Updateimg(picture_id: number, point: number) {
-        const url = this.constants.API_ENDPOINT + '/picture/update/'+picture_id;
-        const body = {
-          score: point,
-        };
-        const headers = new HttpHeaders().set('Content-Type', 'application/json');
-        try {
-          const response = await this.http.put(url, body, { headers: headers }).toPromise();
-          return true;
-        } catch (error) {
-          throw error;
-        }
-      }
+  public async InsertVote(
+    uid: number,
+    picture_id: number,
+    point: number,
+    isWinner: boolean
+  ) {
+    const url = this.constants.API_ENDPOINT + '/vote/insertimg';
+    const body = {
+      uid: uid,
+      picture_id: picture_id,
+      point: point,
+      isWinner: isWinner,
+    };
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    try {
+      const response = await this.http
+        .post(url, body, { headers: headers })
+        .toPromise();
+      return true;
+    } catch (error) {
+      throw error;
+    }
+  }
+  public async Updateimg(picture_id: number, point: number) {
+    const url = this.constants.API_ENDPOINT + '/picture/update/' + picture_id;
+    const body = {
+      point: point,
+    };
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    try {
+      const response = await this.http
+        .put(url, body, { headers: headers })
+        .toPromise();
+      return true;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
